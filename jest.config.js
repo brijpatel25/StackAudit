14fc64ac97cf3410f435c8ddfa77d3aed746da15
brijpatel25 <brijpatel25@gmail.com>
2026-05-24 15:42:45 +0530

    fix: correct jest config syntax

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/\',
+    '^@/(.*)$': '<rootDir>/$1',
   },
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: { module: 'commonjs' } }],
